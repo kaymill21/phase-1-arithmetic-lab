563b10d732bd0726d5cece4188b325ee4a6b9ea1
kayla <kay.ann2000@gmail.com>
2022-03-17 08:23:59 -0600

    commit tree 751f0229910e55cd07f8d70573d4d9301057cb24

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+
+const multiply = num1 * num2;
+    
+const random = Math.ceil(Math.random());
+
+const num3 = 10;
+const num4 = 6;
+const mod = 10 % 6;
+const max = math.max(1 ,2, 20);
